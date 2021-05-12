3024480ca87548ce6b75bd8a982f8310e82f84d0
amagon270 <Amagon270@gmail.com>
2021-05-12 10:43:42 +1000

    added slide transition to persona creation

--- lib/pages/createPersona.dart
+++ lib/pages/createPersona.dart
@@ -10,25 +10,42 @@ class CreatePersona extends StatefulWidget {
   _CreatePersona createState() => _CreatePersona();
 }
 
-class _CreatePersona extends State<CreatePersona> {
+class _CreatePersona extends State<CreatePersona>
+    with SingleTickerProviderStateMixin {
+    
+  AnimationController _controller;
+  Animation<Offset> _offsetAnimation;
+  
   InterviewService interviewService;
   Question currentQuestion;
   QuestionResponse currentQuestionResponse;
   Color currentColor = Colors.white;
   bool canTapNext;
   Widget currentQuestionWidget;
-  int currentQuestionindex;
 
   @override
   void initState() {
     super.initState();
     interviewService = new InterviewService();
     canTapNext = true;
-    currentQuestionindex = 0;
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    )..forward();
+
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(1.5, 0.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
   }
   
   void dispose() {
     super.dispose();
+    _controller.dispose();
   }
 
   _selectAnswer(dynamic answer) {
@@ -68,71 +85,79 @@ class _CreatePersona extends State<CreatePersona> {
             )
           ],
         ),
-        body: SafeArea(
-          child: FutureBuilder(
-            future: interviewService.startSession(),
-            initialData: null,
-            builder: (context, snapshot) {
-              if (snapshot.data == null) {
-                return Text("Loading");
-              } else {
-                //initilizing on the first question
-                if (currentQuestion == null) {
-                  currentQuestion = interviewService.nextQuestion();
-                  currentQuestionResponse = new QuestionResponse(currentQuestion, null);
-                  currentQuestionWidget = currentQuestion?.generateQuestionWidget(selectAnswer: _selectAnswer) ?? Text("Loading");
-                  currentColor = interviewService.getCurrentColor();
-                }
-                return Container(
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(color: currentColor),
-                  child: Column(
-                    children: [
-                      currentQuestionWidget,
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: Container(
+          decoration: BoxDecoration(color: currentColor),
+          child: SlideTransition(
+            position: _offsetAnimation,
+            child: SafeArea(
+              child: FutureBuilder(
+                future: interviewService.startSession(),
+                initialData: null,
+                builder: (context, snapshot) {
+                  if (snapshot.data == null) {
+                    return Text("Loading");
+                  } else {
+                    //initilizing on the first question
+                    if (currentQuestion == null) {
+                      currentQuestion = interviewService.nextQuestion();
+                      currentQuestionResponse = new QuestionResponse(currentQuestion, null);
+                      currentQuestionWidget = currentQuestion?.generateQuestionWidget(selectAnswer: _selectAnswer) ?? Text("Loading");
+                      currentColor = interviewService.getCurrentColor();
+                    }
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(color: currentColor),
+                      child: Column(
                         children: [
-                          //Back button doesn't appear on the first question
-                          currentQuestionindex > 0 ? RaisedButton(
-                            child: Text("Back", style: Theme.of(context).textTheme.button),
-                            onPressed: () {
-                              setState(() {
-                                currentQuestionindex--;
-                                canTapNext = true;
-                                currentQuestionResponse = interviewService.previousQuestion();
-                                currentQuestion = currentQuestionResponse.question;
-                                currentQuestionWidget = currentQuestion?.generateQuestionWidget(selectAnswer: _selectAnswer, startValue: currentQuestionResponse.choice) ?? Text("Loading");
-                              });
-                            }
-                          ) : null,
-                          RaisedButton(
-                            child: Text("Next", style: Theme.of(context).textTheme.button),
-                            //next button can't be tapped unless the user has selected an answer
-                            onPressed: canTapNext ? () {
-                              currentQuestionindex++;
-                              interviewService.answerQuestion(currentQuestion.id, snapshot.data.id, currentQuestionResponse.choice, context.read<User>().id);
-                              setState(() {
-                                currentQuestion = interviewService.nextQuestion();
-                                //TODO - I think i might add a text only question to avoid things like this
-                                if (currentQuestion != InterviewService.blankQuestion) {
-                                  canTapNext = false;
+                          currentQuestionWidget,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              //Back button doesn't appear on the first question
+                              currentQuestion.id != "intro" ? RaisedButton(
+                                child: Text("Back", style: Theme.of(context).textTheme.button),
+                                onPressed: () {
+                                  setState(() {
+                                    _controller.reset();
+                                    _controller.forward();
+                                    canTapNext = true;
+                                    currentQuestionResponse = interviewService.previousQuestion();
+                                    currentQuestion = currentQuestionResponse.question;
+                                    currentQuestionWidget = currentQuestion?.generateQuestionWidget(selectAnswer: _selectAnswer, startValue: currentQuestionResponse.choice) ?? Text("Loading");
+                                  });
                                 }
-                                currentQuestionResponse = new QuestionResponse(currentQuestion, null);
-                                currentQuestionWidget = currentQuestion?.generateQuestionWidget(selectAnswer: _selectAnswer) ?? Text("Loading");
-                              });
-                              if (currentQuestion == InterviewService.endQuestion) {
-                                Navigator.pop(context);
-                              }
-                            } : null,
+                              ) : null,
+                              RaisedButton(
+                                child: Text("Next", style: Theme.of(context).textTheme.button),
+                                //next button can't be tapped unless the user has selected an answer
+                                onPressed: canTapNext ? () {
+                                  interviewService.answerQuestion(currentQuestion.id, snapshot.data.id, currentQuestionResponse.choice, context.read<User>().id);
+                                  setState(() {
+                                    _controller.reset();
+                                    _controller.forward();
+                                    currentQuestion = interviewService.nextQuestion();
+                                    //TODO - I think i might add a text only question to avoid things like this
+                                    if (currentQuestion != InterviewService.blankQuestion) {
+                                      canTapNext = false;
+                                    }
+                                    currentQuestionResponse = new QuestionResponse(currentQuestion, null);
+                                    currentQuestionWidget = currentQuestion?.generateQuestionWidget(selectAnswer: _selectAnswer) ?? Text("Loading");
+                                  });
+                                  if (currentQuestion == InterviewService.endQuestion) {
+                                    Navigator.pop(context);
+                                  }
+                                } : null,
+                              ),
+                            ].where((o) => o != null).toList() //null protection
                           ),
-                        ].where((o) => o != null).toList()
-                      ),
-                    ]
-                  )
-                )
-                ;
-              }
-            },
+                        ]
+                      )
+                    )
+                    ;
+                  }
+                },
+              )
+            )
           )
         )
       )
